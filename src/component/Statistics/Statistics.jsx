763bf04e6e08daf17ab9d129cafbc50ab0b8b217
Nazrul Islam <dobon.islam@icloud.com>
2023-09-24 22:08:39 -0400

    Navbar completed

---
+++ src/component/Statistics/Statistics.jsx
@@ -0,0 +1,11 @@
+
+
+const Statistics = () => {
+    return (
+        <div>
+            <h2>I'm from Statistics</h2>
+        </div>
+    );
+};
+
+export default Statistics;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,39 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './component/Root/Root.jsx';
+import Home from './component/Home/Home.jsx';
+import Donation from './component/Donation/Donation.jsx';
+import Statistics from './component/Statistics/Statistics.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Root></Root>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+      }, 
+      {
+        path:"/donation",
+        element: <Donation></Donation>
+      },
+      {
+        path: "/statistics",
+        element:<Statistics></Statistics>
+      }
+    ]
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </React.StrictMode>,
 )
